bdf1103851a4b540904104b2523baab69f6d2356
Yasser Tahiri <yasserth19@gmail.com>
2021-07-10 22:25:04 +0100

    Feat : Delete the old Directorie Structure :rocket:

--- config/security.py
+++
@@ -1,9 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-
-class JWT(object):
-    JWT_SECRET_KEY = str(os.environ.get("JWT_SECRET"))
-    JWT_BLACKLIST_ENABLED = True
-    JWT_BLACKLIST_TOKEN_CHECKS = ['access', 'refresh']
